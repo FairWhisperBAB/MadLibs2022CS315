508a1aa5b3efcd103d6ce3e1ef7f91d340fdc038
benjaminbriggs <bbriggs2002@gmail.com>
2022-09-22 09:19:51 -0500

    SCOND

--- app/src/main/java/com/example/madlibs2022cs315/MadLibActivity.java
+++ app/src/main/java/com/example/madlibs2022cs315/MadLibActivity.java
@@ -10,6 +10,16 @@ import android.widget.TextView;
 public class MadLibActivity extends AppCompatActivity {
 
     TextView textView;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+    TextView textView8;
+    TextView textView9;
+    TextView textView10;
+    TextView textView11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,40 +28,60 @@ public class MadLibActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
+        Intent intent2 = getIntent();
+        Intent intent3 = getIntent();
+        Intent intent4 = getIntent();
+        Intent intent5 = getIntent();
+        Intent intent6 = getIntent();
+        Intent intent7 = getIntent();
+        Intent intent8 = getIntent();
+        Intent intent9 = getIntent();
+        Intent intent10 = getIntent();
+        Intent intent11 = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message2 = intent2.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message3 = intent3.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message4 = intent4.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message5 = intent5.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message6 = intent6.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message7 = intent7.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message8 = intent8.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message9 = intent9.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message10 = intent10.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message11 = intent11.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
         textView = findViewById(R.id.name_textbox);
         textView.setText(message);
 
-        textView = findViewById(R.id.adjective_textbox);
-        textView.setText(message);
+        textView2 = findViewById(R.id.adjective_textbox);
+        textView2.setText(message2);
 
-        textView = findViewById(R.id.Verb_textbox);
-        textView.setText(message);
+        textView3 = findViewById(R.id.Verb_textbox);
+        textView3.setText(message3);
 
-        textView = findViewById(R.id.sillyWord_textbox);
-        textView.setText(message);
+        textView4 = findViewById(R.id.sillyWord_textbox);
+        textView4.setText(message4);
 
-        textView = findViewById(R.id.noun_textbox);
-        textView.setText(message);
+        textView5 = findViewById(R.id.noun_textbox);
+        textView5.setText(message5);
 
-        textView = findViewById(R.id.nounPlural_textbox);
-        textView.setText(message);
+        textView6 = findViewById(R.id.nounPlural_textbox);
+        textView6.setText(message6);
 
-        textView = findViewById(R.id.verbED_textbox);
-        textView.setText(message);
+        textView7 = findViewById(R.id.verbED_textbox);
+        textView7.setText(message7);
 
-        textView = findViewById(R.id.noun2_textbox);
-        textView.setText(message);
+        textView8 = findViewById(R.id.noun2_textbox);
+        textView8.setText(message8);
 
-        textView = findViewById(R.id.noun3_textbox);
-        textView.setText(message);
+        textView9 = findViewById(R.id.noun3_textbox);
+        textView9.setText(message9);
 
-        textView = findViewById(R.id.verbED2_textbox);
-        textView.setText(message);
+        textView10 = findViewById(R.id.verbED2_textbox);
+        textView10.setText(message10);
 
-        textView = findViewById(R.id.adjective2_textbox);
-        textView.setText(message);
+        textView11 = findViewById(R.id.adjective2_textbox);
+        textView11.setText(message11);
     }
 }
--- app/src/main/java/com/example/madlibs2022cs315/MainActivity.java
+++ app/src/main/java/com/example/madlibs2022cs315/MainActivity.java
@@ -11,9 +11,40 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String EXTRA_MESSAGE;
+    public static String EXTRA_MESSAGE = "1";
+    public static String EXTRA_MESSAGE2 = "2";
+    public static String EXTRA_MESSAGE3 = "3";
+    public static String EXTRA_MESSAGE4 = "4";
+    public static String EXTRA_MESSAGE5 = "5";
+    public static String EXTRA_MESSAGE6 = "6";
+    public static String EXTRA_MESSAGE7 = "7";
+    public static String EXTRA_MESSAGE8 = "8";
+    public static String EXTRA_MESSAGE9 = "9";
+    public static String EXTRA_MESSAGE10 = "10";
+    public static String EXTRA_MESSAGE11 = "11";
     Intent intent;
+    Intent intent2;
+    Intent intent3;
+    Intent intent4;
+    Intent intent5;
+    Intent intent6;
+    Intent intent7;
+    Intent intent8;
+    Intent intent9;
+    Intent intent10;
+    Intent intent11;
+
     String message;
+    String message2;
+    String message3;
+    String message4;
+    String message5;
+    String message6;
+    String message7;
+    String message8;
+    String message9;
+    String message10;
+    String message11;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,57 +61,67 @@ public class MainActivity extends AppCompatActivity {
         message = name_text.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent2 = new Intent(this, MadLibActivity.class);
         EditText adjective_text = (EditText) findViewById(R.id.editTextTextPersonName2);
-        message = adjective_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message2 = adjective_text.getText().toString();
+        intent2.putExtra(EXTRA_MESSAGE2, message2);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent3 = new Intent(this, MadLibActivity.class);
         EditText verb_text = (EditText) findViewById(R.id.editTextTextPersonName3);
-        message = verb_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message3 = verb_text.getText().toString();
+        intent3.putExtra(EXTRA_MESSAGE3, message3);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent4 = new Intent(this, MadLibActivity.class);
         EditText sillyWord_text = (EditText) findViewById(R.id.editTextTextPersonName4);
-        message = sillyWord_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message4 = sillyWord_text.getText().toString();
+        intent4.putExtra(EXTRA_MESSAGE4, message4);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent5 = new Intent(this, MadLibActivity.class);
         EditText noun_text = (EditText) findViewById(R.id.editTextTextPersonName5);
-        message = noun_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message5 = noun_text.getText().toString();
+        intent5.putExtra(EXTRA_MESSAGE5, message5);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent6 = new Intent(this, MadLibActivity.class);
         EditText nounPlural_text = (EditText) findViewById(R.id.editTextTextPersonName6);
-        message = nounPlural_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message6 = nounPlural_text.getText().toString();
+        intent6.putExtra(EXTRA_MESSAGE6, message6);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent7 = new Intent(this, MadLibActivity.class);
         EditText verbED_text = (EditText) findViewById(R.id.editTextTextPersonName7);
-        message = verbED_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message7 = verbED_text.getText().toString();
+        intent7.putExtra(EXTRA_MESSAGE7, message7);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent8 = new Intent(this, MadLibActivity.class);
         EditText noun2_text = (EditText) findViewById(R.id.editTextTextPersonName8);
-        message = noun2_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message8 = noun2_text.getText().toString();
+        intent8.putExtra(EXTRA_MESSAGE8, message8);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent9 = new Intent(this, MadLibActivity.class);
         EditText noun3_text = (EditText) findViewById(R.id.editTextTextPersonName9);
-        message = noun3_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message9 = noun3_text.getText().toString();
+        intent9.putExtra(EXTRA_MESSAGE9, message9);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent10 = new Intent(this, MadLibActivity.class);
         EditText verbED2_text = (EditText) findViewById(R.id.editTextTextPersonName10);
-        message = verbED2_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message10 = verbED2_text.getText().toString();
+        intent10.putExtra(EXTRA_MESSAGE10, message10);
 
-        intent = new Intent(this, MadLibActivity.class);
+        intent11 = new Intent(this, MadLibActivity.class);
         EditText adjective2_text = (EditText) findViewById(R.id.editTextTextPersonName11);
-        message = adjective2_text.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        message11 = adjective2_text.getText().toString();
+        intent11.putExtra(EXTRA_MESSAGE11, message11);
 
         startActivity(intent);
+        startActivity(intent2);
+        startActivity(intent3);
+        startActivity(intent4);
+        startActivity(intent5);
+        startActivity(intent6);
+        startActivity(intent7);
+        startActivity(intent8);
+        startActivity(intent9);
+        startActivity(intent10);
+        startActivity(intent11);
     }
 
 }
